203a2c03a1363bb5485190bd2e8c5d62c1242faf
Justin admin <jyoungbird@fortlewis.edu>
2025-04-30 23:12:57 -0600

    added dark mode and greetings island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://jerboablog.netlify.app/'
+  site: 'https://jerboablog.netlify.app/',
+  integrations: [preact()]
 });
